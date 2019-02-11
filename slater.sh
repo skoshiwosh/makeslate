534ab5d6ea7f6a1fef10dfa70e8c07c33bdbf644
Suzanne Berger <tangle1@mindspring.com>
2019-02-11 11:04:06 -0800

    Add files via upload

---
+++ slater.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#  slater.sh
+#  
+#
+#  Created by Suzanne Berger on 2/07/19.
+#
+MAYAPY=/Applications/Autodesk/maya2017/Maya.app/Contents/bin/mayapy
+SLATERPY=/Users/suzanneberger/Documents/dev/slater/slater.py
+#
+$MAYAPY $SLATERPY $*
