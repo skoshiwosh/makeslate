7c6c35db66ee4482999b30dbc96c4508968e6fd5
Suzanne Berger <tangle1@mindspring.com>
2019-02-11 18:57:21 -0800

    Add files via upload

--- slater.py
+++ slater.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
-'''
-    Create a slate for vfx
+"""
+    Create a slate for visual effects dailies
+    
     File name: slater.py
     Author: Suzanne Berger
+    Email: zanefx7@gmail.com
     Date created: 02/07/2019
     Python Version: 2.7
-    '''
+"""
 
 import sys
 import os
@@ -25,40 +27,38 @@ VERSION = "V01"
 logging.basicConfig(level=logging.INFO)
 logging.info( " %s Version %s" % (sys.argv[0], VERSION))
 
-TESTSHOW = "FACING THE BEAST"
+# use as default for artist entry
+if sys.platform == "win32":
+    USERNAME = os.getenv("USERNAME")
+else:
+    USERNAME = os.getenv("USER")
 
+# replace these hard-coded file paths with appropriate to facility
 PROGPATH = "/Users/suzanneberger/Documents/dev/slater"
 BGSLATE = os.path.join(PROGPATH,"bgslate.png")
-THUMBNAIL = os.path.join(PROGPATH, "slate_thumbnail.jpg")
-TESTSLATE = os.path.join(PROGPATH, "testSlate_02.jpg")
+
 
 #########################################################
 # Slater
 #########################################################
 
 class Slater(QtWidgets.QWidget):
-
-    def __init__(self, parent=None, movie=None, artist=None):
-        
+    """This widget displays slate show/shot data along with thumbnail image and can be saved as an image file."""
+    
+    def __init__(self, parent=None, movie=None, shot=None, artist=None):
         super(Slater, self).__init__(parent)
         
-        self.thumb_file = None
-        
         self.movie = movie
-        if not self.movie:
-            self.movie = os.getenv("SHOW")
-            if not self.movie:
-                self.movie = "!!! Enter Movie Title  !!!"
-
+        if not self.movie:      # remind the user to enter show title
+            self.movie = "!!! Enter Movie Title  !!!"
+        self.shot = shot
         self.artist = artist
-        if not self.artist:
-            self.artist = os.getenv("LOGNAME")      # this env depends on platform
 
         self.initUI()
-        #self.connectSignals()
         self.show()
 
     def initUI(self):
+        """Create and initialize widgets and layouts for this object."""
         self.setGeometry(100, 80, 1280, 720)
         self.setFixedSize(1280, 720)
         self.setWindowTitle('Slater')
@@ -73,6 +73,7 @@ class Slater(QtWidgets.QWidget):
         palette.setBrush(QtGui.QPalette.Window, sImage)
         self.setPalette(palette)
 
+        # thumbnail image will be contained in this QLabel
         self.thumbnail_label = QtWidgets.QLabel("LOAD THUMBNAIL")
         self.thumbnail_label.setFixedSize(500, 280)
         self.thumbnail_label.setAlignment(QtCore.Qt.AlignCenter)
@@ -83,12 +84,13 @@ class Slater(QtWidgets.QWidget):
         thumbnail_layout.addSpacing(500)
         thumbnail_layout.addWidget(self.thumbnail_label)
 
-        self.title_label = QtWidgets.QLabel(self.movie)
-        self.title_label.setFixedSize(400,51)
-        self.title_label.setFont(QtGui.QFont("Times", 36, QtGui.QFont.Bold))
-        self.title_label.setStyleSheet('QLabel { color: blue }')
-        self.title_label.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+        self.movie_label = QtWidgets.QLabel(self.movie)
+        self.movie_label.setFixedSize(400,51)
+        self.movie_label.setFont(QtGui.QFont("Times", 36, QtGui.QFont.Bold))
+        self.movie_label.setStyleSheet('QLabel { color: blue }')
+        self.movie_label.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
 
+        # these lineEdits allow user input although some can also be input or initialized from SlateWin object
         self.shot_lineEdit, shot_layout = self.mk_lineEdit(QtWidgets.QLabel('shot: '))
         self.filename_lineEdit, filename_layout = self.mk_lineEdit(QtWidgets.QLabel('filename: '))
         self.artist_lineEdit, artist_layout = self.mk_lineEdit(QtWidgets.QLabel('artist: '))
@@ -96,14 +98,18 @@ class Slater(QtWidgets.QWidget):
         self.date_lineEdit, date_layout = self.mk_lineEdit(QtWidgets.QLabel('date: '))
         self.notes_lineEdit, notes_layout = self.mk_lineEdit(QtWidgets.QLabel('notes:   '), 175, 600, 520)
 
-        self.artist_lineEdit.setText(self.artist)
+        if self.artist:
+            self.artist_lineEdit.setText(self.artist)
+        if self.shot:
+            self.shot_lineEdit.setText(self.shot)
         self.date_lineEdit.setText(str(date.today()))
 
+        # set overall layout to make a nice slate image when grabbed as a QPixmap
         slateboxLayout = QtWidgets.QVBoxLayout()
         slateboxLayout.addSpacing(10)
         slateboxLayout.addLayout(thumbnail_layout)
         slateboxLayout.addStretch()
-        slateboxLayout.addWidget(self.title_label)
+        slateboxLayout.addWidget(self.movie_label)
         slateboxLayout.addSpacing(10)
         slateboxLayout.addLayout(shot_layout)
         slateboxLayout.addLayout(filename_layout)
@@ -118,11 +124,11 @@ class Slater(QtWidgets.QWidget):
         mainLayout.addWidget(self.slatebox)
         self.setLayout(mainLayout)
 
-
     def closeEvent(self, event):
         event.accept()
 
     def mk_lineEdit(self, this_label, label_width=140, line_width=200, layout_rightspace=850):
+        """Make labels, lineEdits and layouts for slate entry fields."""
         this_label.setFixedSize(label_width, 27)
         this_label.setFont(QtGui.QFont("Times", 18, QtGui.QFont.Normal))
         this_label.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
@@ -141,66 +147,88 @@ class Slater(QtWidgets.QWidget):
         return this_lineEdit, this_layout
 
     def set_thumbnail(self, thumb_file):
+        """Load thumbnail image file into a QPixMap and set the corresponding QLabel."""
         self.thumbnail = QtGui.QPixmap(thumb_file).scaled(500, 280)
         self.thumbnail_label.setPixmap(self.thumbnail)
 
+
+############################################################
+# SlaterWin
+############################################################
+
 class SlaterWin(QtWidgets.QWidget):
+    """This widget creates a Slater widget object and it contains input fields and buttons for 
+       initializing and saving a slate image file.
+    """
     
-    def __init__(self, parent=None, movie=None):
+    def __init__(self, parent=None):
         super(SlaterWin, self).__init__(parent)
         
-        self.movie = movie
-        if not self.movie:
-            self.movie = os.getenv("SHOW")
-            if not self.movie:
-                self.movie = " !!! Enter Movie Title  !!!"
+        # optionally set these environment variables outside script to initialize corresponding widgets
+        self.movie = os.getenv("SHOW")
+        self.shot = os.getenv("SHOT")
+        self.artist = os.getenv("ARTIST")
+        if not self.artist:
+            self.artist = USERNAME
         
         self.initUI()
 
     def initUI(self):
-    
+        """Create and initialize widgets and layouts for this object."""
         self.setGeometry(100, 840, 1280, 100)
         self.setWindowTitle('Make Slate')
     
-        self.thumbnail_button = QtWidgets.QPushButton("Load Thumbnail")
-        self.status_lineEdit = QtWidgets.QLineEdit()
-        self.status_lineEdit.setMinimumWidth(800)
-        self.status_lineEdit.setText("Status:")
-        self.status_lineEdit.setReadOnly(True)
-        self.save_button = QtWidgets.QPushButton("Save Slate")
-        self.close_button = QtWidgets.QPushButton("Close")
-
-
-        slatebuttons_layout = QtWidgets.QHBoxLayout()
-        slatebuttons_layout.addWidget(self.thumbnail_button)
-        slatebuttons_layout.addWidget(self.status_lineEdit)
-        slatebuttons_layout.addStretch()
-        slatebuttons_layout.addWidget(self.save_button)
-        slatebuttons_layout.addWidget(self.close_button)
+        self.setStyleSheet("QPushButton {color: black; min-width: 100px; min-height: 35px;}")
         
+        # create show title, shot and artist labels and lineEdits for input into Slater widget
         self.show_label = QtWidgets.QLabel("Show: ")
         self.show_lineEdit = QtWidgets.QLineEdit()
-        if os.getenv("SHOW"):
-            self.show_lineEdit.setText(os.getenv("SHOW"))
+        if self.movie:
+            self.show_lineEdit.setText(self.movie)
+        
         self.shot_label = QtWidgets.QLabel("Shot: ")
         self.shot_lineEdit = QtWidgets.QLineEdit()
-        self.artist_label = QtWidgets.QLabel("Artist: ")
-        self.artist_lineEdit = QtWidgets.QLineEdit()
-        self.artist_lineEdit.setText(os.getenv("LOGNAME"))      # this env depends on platform
+        if self.shot:
+            self.shot_lineEdit.setText(self.shot)
         
-        slatedata_layout = QtWidgets.QHBoxLayout()
-        slatedata_layout.addWidget(self.show_label)
-        slatedata_layout.addWidget(self.show_lineEdit)
-        slatedata_layout.addWidget(self.shot_label)
-        slatedata_layout.addWidget(self.shot_lineEdit)
-        slatedata_layout.addWidget(self.artist_label)
-        slatedata_layout.addWidget(self.artist_lineEdit)
-       
+        self.artist_label = QtWidgets.QLabel("Artist: ")
+        self.artist_lineEdit = QtWidgets.QLineEdit(self.artist)
+        
+        
+        self.thumbnail_button = QtWidgets.QPushButton("Load Thumbnail")
+        self.save_button = QtWidgets.QPushButton("Save Slate")
+        
+        slate_input_layout = QtWidgets.QHBoxLayout()
+        slate_input_layout.addWidget(self.show_label)
+        slate_input_layout.addWidget(self.show_lineEdit)
+        slate_input_layout.addWidget(self.shot_label)
+        slate_input_layout.addWidget(self.shot_lineEdit)
+        slate_input_layout.addWidget(self.artist_label)
+        slate_input_layout.addWidget(self.artist_lineEdit)
+        slate_input_layout.addSpacing(40)
+        slate_input_layout.addWidget(self.thumbnail_button)
+        slate_input_layout.addWidget(self.save_button)
+
+        # create widgets and layout to be placed at bottom of window
+        status_label = QtWidgets.QLabel("Status: ")
+        self.status_lineEdit = QtWidgets.QLineEdit()
+        self.status_lineEdit.setMinimumWidth(850)
+        self.status_lineEdit.setReadOnly(True)
+        self.close_button = QtWidgets.QPushButton("Close")
+        
+        slate_bottom_layout = QtWidgets.QHBoxLayout()
+        slate_bottom_layout.addWidget(status_label)
+        slate_bottom_layout.addWidget(self.status_lineEdit)
+        slate_bottom_layout.addStretch()
+        slate_bottom_layout.addWidget(self.close_button)
+
+        # add layouts to overall window layout
         slatewin_layout = QtWidgets.QVBoxLayout()
-        slatewin_layout.addLayout(slatedata_layout)
-        slatewin_layout.addLayout(slatebuttons_layout)
+        slatewin_layout.addLayout(slate_input_layout)
+        slatewin_layout.addLayout(slate_bottom_layout)
         self.setLayout(slatewin_layout)
         
+        # connect signal to slots for updating slate
         self.thumbnail_button.clicked.connect(self.loadthumb)
         self.save_button.clicked.connect(self.saveslate)
         self.close_button.clicked.connect(self.close)
@@ -209,49 +237,54 @@ class SlaterWin(QtWidgets.QWidget):
         self.artist_lineEdit.returnPressed.connect(self.on_artistEdit)
 
         self.show()
-        self.slateframe = Slater()
+        self.slateframe = Slater(movie=self.movie, shot=self.shot, artist=self.artist)
 
     def closeEvent(self, event):
+        """Close this widget and Slater widget."""
         self.slateframe.close()
         event.accept()
+    
+    
+############################################################
+# slots
+############################################################
 
     def loadthumb(self):
-        #self.slateframe.set_thumbnail(THUMBNAIL)
         thumbfile = QtWidgets.QFileDialog.getOpenFileName(self,'Load Thumbnail Image File',
                                                           PROGPATH,
                                                           "Image files (*.jpg *.png *.tif)")[0]
         self.slateframe.set_thumbnail(thumbfile)
-        self.status_lineEdit.setText("Status: Loaded Thumbnail Image from File: %s" % thumbfile)
+        self.status_lineEdit.setText("Loaded Thumbnail Image from File: %s" % thumbfile)
 
     def saveslate(self):
         this_slate = self.slateframe.grab()
-        #slate_file = TESTSLATE
         slatefile = QtWidgets.QFileDialog.getSaveFileName(self, "Save Slate to File",
                                                    PROGPATH,
                                                    "Images (*.jpg *.png *.tif)")[0]
         this_slate.save(slatefile)
-        self.status_lineEdit.setText("Status: Saved Slate Image to File: %s" % slatefile)
+        self.status_lineEdit.setText("Saved Slate Image to File: %s" % slatefile)
 
     def on_showEdit(self):
-        show_title = self.show_lineEdit.text()
-        self.slateframe.title_label.setText(show_title)
+        """Set new movie title in Slater widget's movie_label."""
+        self.movie = self.show_lineEdit.text()
+        self.slateframe.movie_label.setText(self.movie)
 
     def on_shotEdit(self):
-        shot = self.shot_lineEdit.text()
-        self.slateframe.shot_lineEdit.setText(shot)
+        """Set new shot title in Slater widget's shot_lineEdit."""
+        self.shot = self.shot_lineEdit.text()
+        self.slateframe.shot_lineEdit.setText(self.shot)
 
     def on_artistEdit(self):
-        artist = self.artist_lineEdit.text()
-        self.slateframe.artist_lineEdit.setText(artist)
+        """Set new artist name in Slater widget's artist_lineEdit."""
+        self.artist = self.artist_lineEdit.text()
+        self.slateframe.artist_lineEdit.setText(self.artist)
 
 
-#########################################################
+############################################################
 # main
-#########################################################
+############################################################
 
 if __name__ == '__main__':
-    
-    os.environ["SHOW"] = TESTSHOW     # test only env SHOW variable should be set outside
     
     app = QtWidgets.QApplication(sys.argv)
     slaterwin = SlaterWin()
